7744a036381a955d7b010360b9b5b5866a587fbe
michael <devking0115@gmail.com>
2024-08-16 17:35:29 +0100

    init project

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './Education';
+export * from './Experience';
+export * from './Layout';
+export * from './Navbar';
+export * from './Project';
+export * from './Skills';
+export * from './Welcome';
